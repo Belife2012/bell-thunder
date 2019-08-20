75a6224bf31c960238be5767f775ee65fad837fc
Belife2012 <790541750@qq.com>
2019-08-20 19:17:18 +0800

    更改更新程序的文件

--- Thunder_lib/example/manufacture/manufacture.ino
+++ Thunder_lib/example/manufacture/manufacture.ino
@@ -2,35 +2,51 @@
 
 void setup()
 {
-  // initial thunder-car all hareware resource
-  Bell_Thunder.Setup_All();
+	// initial thunder-car all hareware resource
+	Bell_Thunder.Setup_All();
+	Bell_Thunder.Reset_Process_Status();
 
-  // 舵机位置初始化
-  Motor_Servo.Servo_Turn(1, 90); //参数1--> 舵机编号；参数2 --> 角度[%](0~180)
-  Motor_Servo.Servo_Turn(2, 90); //参数1--> 舵机编号；参数2 --> 角度[%](0~180)
+	// 舵机位置初始化
+	Motor_Servo.Servo_Turn(1, 90); //参数1--> 舵机编号；参数2 --> 角度[%](0~180)
+	Motor_Servo.Servo_Turn(2, 90); //参数1--> 舵机编号；参数2 --> 角度[%](0~180)
 }
 void loop()
 {
-  Programs_System();
+	Programs_System();
+	vTaskDelay(pdMS_TO_TICKS(10)); // 如果不增加vTaskDelay，此循环出现不工作的情况；
 }
 
 #ifdef COMPETITION_FW_001
-
 void Program_AutoCtrl()
-{}
-
+{
+	// 创建赛前一分钟的程序
+	Create_New_Loop_AutoCtrl(PROGRAM_USER_1, setup_AutoCtrl_1, loop_AutoCtrl_1);
+}
+void setup_AutoCtrl_1()
+{
+}
+void loop_AutoCtrl_1()
+{
+}
 #endif
+
 void Program_1()
-{}
+{
+	Bell_Thunder.Set_Ble_Type(BLE_TYPE_SERVER); // ThunderGo 模式，进入BLE Server
+	Bell_Thunder.Set_Need_Communication(true);
+}
 void Program_2()
-{}
+{
+}
 void Program_3()
-{}
+{
+}
 void Program_4()
-{}
+{
+}
 
 void Program_ThunderGo()
 {
-  Bell_Thunder.Set_Ble_Type(BLE_TYPE_SERVER); // ThunderGo 模式，进入BLE Server
-  Bell_Thunder.Set_Need_Communication(true);
+	Bell_Thunder.Set_Ble_Type(BLE_TYPE_SERVER); // ThunderGo 模式，进入BLE Server
+	Bell_Thunder.Set_Need_Communication(true);
 }
